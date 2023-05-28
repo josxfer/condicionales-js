4442b7b9310be589abbe5f330ca8ed8be7fc725e
Jose Espinosa <josxfer@gmail.com>
2023-05-28 18:33:18 -0500

    Me Falta sólo el ultimo ejercicio

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let opcion = parseInt(prompt("Ingresa el numero del ejercicio que deseas hacer: "));
 
-if (opcion >= 1 & opcion <= 12) {
+if (opcion >= 1 & opcion <= 11) {
     if (opcion === 1) {
         let bello = prompt("¿Eres bellisimo/a?");
         bello = bello.toUpperCase();
@@ -101,6 +101,96 @@ if (opcion >= 1 & opcion <= 12) {
             document.write("Error");
         }
     }
+    if (opcion === 9) {
+        let adicion = prompt("¿Quieres topping para tu helado?, por favor responde si o no: ");
+        adicion = adicion.toUpperCase();
+        if (adicion === "SI") {
+            let topping = prompt("Escribe a continuacion cuál topping quieres para tu helado (Oreo, KitKat o Brownie): ");
+            topping = topping.toUpperCase();
+            if (topping === "OREO") {
+                document.write("Tu helado cuesta 60 MXN");
+            }else if (topping === "KITKAT") {
+                document.write("Tu helado cuesta 65 MXN");
+            }else if (topping === "BROWNIE") {
+                document.write("Tu helado cuesta 70 MXN");
+            }else{
+                document.write("No tenemos este topping, lo sentimos. Tu helado cuesta 50 MXN");
+            }
+        }else if (adicion === "NO") {
+            document.write("Tu helado cuesta 50 MXN")
+        }else{
+            document.write("Ingresa una respuesta válida")
+        }
+    }
+    if (opcion === 10) {
+        let course= 4999;
+        let carrera = 3999;
+        let master = 2999;
+        let programa = prompt("Por favor escribe cual programa deseas estudiar (Course, Carrera o Master): ");
+        programa = programa.toUpperCase();
+        if (programa === "COURSE" || programa === "CARRERA" || programa === "MASTER") {
+            descuento = prompt("¿Tienes alguna beca de descuento? (Responde Si o No): ");
+            descuento = descuento.toUpperCase();
+            if (descuento === "SI") {
+                let beca = prompt("Cual beca tienes (Facebook, Google o Jesua) ");
+                beca = beca.toUpperCase();
+                if (programa === "COURSE" & beca === "FACEBOOK") {
+                    let val_mensual = course -((course/100)*20);
+                    let val_total = val_mensual * 2;
+                    document.write("El valor a pagar mensual por el programa Course con el descuento incluido son: " + val_mensual.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por dos meses.");
+                }else if (programa === "COURSE" & beca === "GOOGLE") {
+                    let val_mensual = course -((course/100)*15);
+                    let val_total = val_mensual * 2;
+                    document.write("El valor a pagar mensual por el programa Course con el descuento incluido son: " + val_mensual.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por dos meses.");
+                }else if (programa === "COURSE" & beca === "JESUA") {
+                    let val_mensual = course -((course/100)*50);
+                    let val_total = val_mensual * 2;
+                    document.write("El valor a pagar mensual por el programa Course con el descuento incluido son: " + val_mensual.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por dos meses.");
+                }else if (programa === "CARRERA" & beca === "FACEBOOK") {
+                    let val_mensual = carrera -((carrera/100)*20);
+                    let val_total = val_mensual * 6;
+                    document.write("El valor a pagar mensual por el programa Carrera con el descuento incluido son: " + val_mensual.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por seis meses.");
+                }else if (programa === "CARRERA" & beca === "GOOGLE") {
+                    let val_mensual = carrera -((carrera/100)*15);
+                    let val_total = val_mensual * 6;
+                    document.write("El valor a pagar mensual por el programa Carrera con el descuento incluido son: " + val_mensual.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por seis meses.");
+                }else if (programa === "CARRERA" & beca === "JESUA") {
+                    let val_mensual = carrera -((carrera/100)*50);
+                    let val_total = val_mensual * 6;
+                    document.write("El valor a pagar mensual por el programa Carrera con el descuento incluido son: " + val_mensual.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por seis meses.");
+                }else if (programa === "MASTER" & beca === "FACEBOOK") {
+                    let val_mensual = master -((master/100)*20);
+                    let val_total = val_mensual * 12;
+                    document.write("El valor a pagar mensual por el programa Master con el descuento incluido son: " + val_mensual.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por doce meses.");
+                }else if (programa === "MASTER" & beca === "GOOGLE") {
+                    let val_mensual = master -((master/100)*15);
+                    let val_total = val_mensual * 12;
+                    document.write("El valor a pagar mensual por el programa Master con el descuento incluido son: " + val_mensual.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por doce meses.");
+                }else if (programa === "MASTER" & beca === "JESUA") {
+                    let val_mensual = master -((master/100)*50);
+                    let val_total = val_mensual * 12;
+                    document.write("El valor a pagar mensual por el programa Master con el descuento incluido son: " + val_mensual.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por doce meses.");
+                }else{
+                    document.write("Este no es un descuento válido");
+                }
+            }else if(descuento === "NO"){
+                if (programa === "COURSE") {
+                    let val_total = course * 2;
+                    document.write("El valor a pagar mensual por el programa Course son: " + course.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por dos meses.");
+                }else if (programa === "CARRERA") {
+                    let val_total = carrera * 6;
+                    document.write("El valor a pagar mensual por el programa Carrera son: " + carrera.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por seis meses.");
+                }else if (programa === "MASTER") {
+                    let val_total = master * 12;
+                    document.write("El valor a pagar mensual por el programa Master son: " + master.toFixed(2) + " MXN" + "<br>" + "El valor a pagar por el total del programa son: " + val_total.toFixed(2) + " MXN por doce meses.");
+                }
+            }else{
+                document.write("Por favor ingresa una opción válida (Si o No)")
+            }
+        }else{
+            document.write("Por favor ingresa una opción Válida (Course, Carrera o Master)")
+        }
+    }
 }else{
-    alert("Ingresa un número válido entre el 1 y el 12");
+    alert("Ingresa un número válido entre el 1 y el 11");
 }
